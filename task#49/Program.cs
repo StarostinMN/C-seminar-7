8c5064c88e75b9d7589ba091834877c91c8c3177
Mikhail Starostin <starostin.mihail@mail.ru>
2022-12-13 23:25:28 +0300

    add task#49 my option

---
+++ task#49/Program.cs
@@ -0,0 +1,80 @@
+﻿/* Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+Например, изначально массив
+выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Новый массив будет выглядеть 
+вот так:
+1 4 7 2
+5 9 2 9 
+8 4 4 4 */
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимально возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите максимально возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(rows, columns, min, max);
+
+PrintArray(array);
+
+Console.WriteLine("--------------------------------------------------------");
+
+int[,] squaresOfEvenArrayIndexes = ComputeSquaresOfEvenArrayIndexes(array);
+
+PrintArray(squaresOfEvenArrayIndexes);
+
+int[,] FillArray(int ArrayRows, int ArrayColumns, int minValue, int maxValue)
+{
+    int[,] filledArray = new int[ArrayRows, ArrayColumns];
+    Random random = new Random();
+    for (int i = 0; i < ArrayRows; i++)
+    {
+        for (int j = 0; j < ArrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return filledArray;
+}
+
+int[,] ComputeSquaresOfEvenArrayIndexes(int[,] InputArray)
+{
+    int[,] SquaresOfEvenArrayIndexes = new int[InputArray.GetLength(0), InputArray.GetLength(1)];
+    for (int i = 0; i < InputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < InputArray.GetLength(1); j++)
+        {
+             if ((i % 2) == 0 && (j % 2) == 0)
+             {
+                SquaresOfEvenArrayIndexes[i, j] = InputArray[i, j] * InputArray[i, j];
+             }
+             else
+             {
+                SquaresOfEvenArrayIndexes[i, j] = InputArray[i, j];
+             }
+        }
+    }
+    return SquaresOfEvenArrayIndexes;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
